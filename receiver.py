6d213eda85c78e627c736a56f3b98dd5c1418ca1
Kris Schueller <schkris@vt.edu>
2023-11-29 12:07:15 -0500

    Working on Decoding

--- receiver.py
+++ receiver.py
@@ -1,4 +1,6 @@
 import numpy as np
+import math
+import struct
 from bchlib import BCH
 
 def bpsk_demodulation(modulated_data, carrier_frequency, time_points):
@@ -6,22 +8,38 @@ def bpsk_demodulation(modulated_data, carrier_frequency, time_points):
     threshold = 0.0
     # Number of bits to be found
     info_length = int(np.ceil(time_points[len(time_points) - 1] * carrier_frequency))
-    print("Info Length: ", info_length)
+    # print("Info Length: ", info_length)
 
     # Create a carrier and shifted carrier and compare both to data values to determine binary
+    base_carrier = np.sin(2 * np.pi * carrier_frequency * time_points)
+    shifted_carrier = np.sin(2 * np.pi * carrier_frequency * time_points + np.pi)
+    # print("Base Carrier Length: ", len(base_carrier))
+    # print("Base Carrier: ", base_carrier)
+    # print("Shifted Carrier Length: ", len(shifted_carrier))
+    # print("Shifted Carrier: ", shifted_carrier)
+
+    # Initialize demodulated_data array
+    demodulated_data = np.zeros(info_length, dtype=int)
+
     for i in range(info_length):
         # Calculate the index for each bit
-        index = int((i + 0.5) * len(time_points) / info_length)
-        
-        # Generate sinusoids for the current bit
-        base_carrier = np.sin(2 * np.pi * carrier_frequency * time_points)
-        shifted_carrier = np.sin(2 * np.pi * carrier_frequency * time_points + np.pi)
+        index = int((i + 0.33) * len(time_points) / info_length)
 
-        # Compare with generated sinusoids
-        if np.dot(modulated_data[index], base_carrier) > threshold:
+        # print("Index: ", index, "Time: ", time_points[index], "Value: ", modulated_data[index])
+
+        # Calculate distances from both carriers
+        distance_base = np.abs(modulated_data[index] - base_carrier[index])
+        distance_shifted = np.abs(modulated_data[index] - shifted_carrier[index])
+        # print("Base: ", distance_base, "Shifted: ", distance_shifted)
+
+        # Determine the demodulated bit based on minimum distance
+        if distance_base < distance_shifted:
             demodulated_data[i] = 0
         else:
             demodulated_data[i] = 1
+
+    # print("Demodulated Data Length: ", len(demodulated_data))
+    # print("Demodulated Data:", demodulated_data)
 
     return demodulated_data
 
@@ -39,12 +57,19 @@ modulated_signal = transmission_data[:, 1]
 # BPSK Demodulation
 threshold_value = 0.0  # Adjust the threshold as needed
 demodulated_data = bpsk_demodulation(modulated_signal, 6e9, time_points)
-print("Demod Data Length: ", len(demodulated_data))
-print("Data: ", demodulated_data[0:])
+# print("Demod Data Length: ", len(demodulated_data))
+# print("Data: ", demodulated_data[0:])
+integer_value = int(''.join(map(str, demodulated_data)), 2)
+byte_data = integer_value.to_bytes((len(demodulated_data) + 7) // 8, byteorder='big')
+print("Data Length: ", len(bin(int.from_bytes(byte_data, "big"))))
+print("Demod Data: ", bin(int.from_bytes(byte_data, "big")))
 
 # BCH Decoding
-# bch = BCH(t=int(1), m=int(1))
-# decoded_data = bch.decode(demodulated_data)
+num_errors = math.ceil(len(bin(int.from_bytes(byte_data, "big"))) * 0.1)
+m = math.ceil(math.log2(len(bin(int.from_bytes(byte_data, "big"))) + 1))
+bch = BCH(t=int(num_errors), m=int(m))
+decoded_data = bch.decode(demodulated_data)
+print("Decoded Data: ", bin(int.from_bytes(decoded_data, "big")))
 
 # Deserialize data
 # final_message = deserialize_data(decoded_data)
